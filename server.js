f86969b738688d068cc64a476c4c4bb8e5c12899
Tupshin Zhu <tup@ad.unc.edu>
2023-05-09 03:58:05 -0400

    no json

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ const args = minimist(process.argv.slice(2));
 const app = express();
 const port = args["port"] || 5000
 
-// Set up middleware to parse JSON and URL-encoded request bodies
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 
 // Define route for root endpoint, returns 200 OK
 app.get("/app/", (req, res) => {
@@ -23,42 +20,42 @@ app.get("/app/", (req, res) => {
 
 // Define route for RPS endpoint, returns results of RPS function as JSON
 app.get("/app/rps/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps()));
+    res.status(200).send(rps());
 })
 
 // Define route for RPSLS endpoint, returns results of RPSLS function as JSON
 app.get("/app/rpsls/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls()));
+    res.status(200).send(rpsls());
 })
 
 // Define route for playing RPS with query parameter, returns results of RPS function as JSON
 app.get("/app/rps/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+    res.status(200).send(rps(req.query.shot));
 })
 
 // Define route for playing RPSLS with query parameter, returns results of RPSLS function as JSON
 app.get("/app/rpsls/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+    res.status(200).send(rpsls(req.query.shot));
 })
 
 // Define route for playing RPS with POST request, returns results of RPS function as JSON
 app.post("/app/rps/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+    res.status(200).send(rps(req.body.shot));
 })
 
 // Define route for playing RPSLS with POST request, returns results of RPSLS function as JSON
 app.post("/app/rpsls/play/", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+    res.status(200).send(rpsls(req.body.shot));
 })
 
 // Define route for playing RPS with URL parameter, returns results of RPS function as JSON
-app.get("/app/rps/play/:shot", (req, res) => {
-    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+app.get("/app/rps/play/:arg", (req, res) => {
+    res.status(200).send(rps(req.params.arg));
 })
 
 // Define route for playing RPSLS with URL parameter, returns results of RPSLS function as JSON
-app.get("/app/rpsls/play/:shot", (req, res) => {
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+app.get("/app/rpsls/play/:arg", (req, res) => {
+    res.status(200).send(rpsls(req.params.arg));
 })
 
 // Define route for all other requests, returns 404 NOT FOUND
