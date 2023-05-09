498351864b58b23667aafce98d3eee957955af28
Tupshin Zhu <tup@ad.unc.edu>
2023-05-09 02:45:51 -0400

    no router

--- server.js
+++ server.js
@@ -1,57 +1,75 @@
+// Import required libraries
+import minimist from "minimist"
 import express from "express"
-import { rpsls, rps } from "./lib/rpsls.js"
 
-// Create a new Express application instance
-const app = express()
+// Import functions from rpsls.js module
+import { rpsls, rps } from "./lib/rpsls.js";
 
-// Use the PORT environment variable, or default to 5000 if not set
-const port = process.env.PORT || 5000
+// Parse command line arguments
+const args = minimist(process.argv.slice(2));
 
-// Set up middleware to parse request bodies as JSON
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+// Create Express app and set port number
+const app = express();
+const port = args["port"] || 5000
 
-// Create a new router instance
-const router = express.Router()
+// Set up middleware to parse JSON and URL-encoded request bodies
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
-// Define routes for Rock, Paper, Scissors and Rock, Paper, Scissors, Lizard, Spock games
-
-// Route for playing Rock, Paper, Scissors game
-router.get("/rps", (req, res) => {
-  res.status(200).send(JSON.stringify(rps()))
+// Define route for root endpoint, returns 200 OK
+app.get("/app/", (req, res) => {
+    res.status(200).send('200 OK');
 })
 
-// Route for playing Rock, Paper, Scissors, Lizard, Spock game
-router.get("/rpsls", (req, res) => {
-  res.status(200).send(JSON.stringify(rpsls()))
+// Define route for RPS endpoint, returns results of RPS function as JSON
+app.get("/app/rps/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
 })
 
-// Route for playing Rock, Paper, Scissors game with a specified move
-router.get("/rps/play/:shot", (req, res) => {
-  res.status(200).send(JSON.stringify(rps(req.params.shot)))
+// Define route for RPSLS endpoint, returns results of RPSLS function as JSON
+app.get("/app/rpsls/", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
 })
 
-// Route for playing Rock, Paper, Scissors, Lizard, Spock game with a specified move
-router.get("/rpsls/play/:shot", (req, res) => {
-  res.status(200).send(JSON.stringify(rpsls(req.params.shot)))
+// Define route for playing RPS with query parameter, returns results of RPS function as JSON
+app.get("/app/rps/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
 })
 
-// Route for playing Rock, Paper, Scissors game via a POST request with a specified move
-router.post("/rps/play", (req, res) => {
-  res.status(200).send(JSON.stringify(rps(req.body.shot)))
+// Define route for playing RPSLS with query parameter, returns results of RPSLS function as JSON
+app.get("/app/rpsls/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 })
 
-// Route for playing Rock, Paper, Scissors, Lizard, Spock game via a POST request with a specified move
-router.post("/rpsls/play", (req, res) => {
-  res.status(200).send(JSON.stringify(rpsls(req.body.shot)))
+// Define route for playing RPS with POST request, returns results of RPS function as JSON
+app.post("/app/rps/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
 })
 
-// Mount the router at the /app route
-app.use("/app", router)
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+// Define route for playing RPSLS with POST request, returns results of RPSLS function as JSON
+app.post("/app/rpsls/play/", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
 })
+
+// Define route for playing RPS with URL parameter, returns results of RPS function as JSON
+app.get("/app/rps/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+})
+
+// Define route for playing RPSLS with URL parameter, returns results of RPSLS function as JSON
+app.get("/app/rpsls/play/:shot", (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
+
+// Define route for all other requests, returns 404 NOT FOUND
+app.get("*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
+
+// Start server and listen on specified port
+app.listen(port, () =>{
+    console.log('Server listening on port ' + port);
+})
+
 
 
